732244666fc57e462430ba8bff8ff8638f29705c
S-a-m-s-o-n-D-a-n-i-e-l <90248074+S-a-m-s-o-n-D-a-n-i-e-l@users.noreply.github.com>
2023-03-31 09:28:51 +0530

    Create 1470. Shuffle the Array.java

---
+++ 1470. Shuffle the Array.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] shuffle(int[] nums, int n) {
+           int[] arr=new int[nums.length];
+           int c=0;
+           for(int i=0;i<n;i++)
+           {
+               arr[c++]=nums[i];
+                arr[c++]=nums[n+i];
+           }
+           return arr;
+    }
+}
